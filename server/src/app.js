096370199512138de0a8df7c50b8c724a2fc44b1
Aakashraz <aakashraz@outlook.com>
2024-07-15 01:21:49 +0545

    HTTP request logging middleware added

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router')
 
@@ -9,6 +10,9 @@ const planetsRouter = require('./routes/planets/planets.router')
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+
+// popular HTTP request logger middleware for Node.js.
+app.use(morgan('combined'));
 
 // to parse any incoming JSON from the body of incoming requests
 app.use(express.json());
